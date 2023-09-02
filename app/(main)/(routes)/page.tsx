94ca061774f9b658b31e7758234c51a8f218f9b1
Diky Arif Febriyanto <dikyariff97@gmail.com>
2023-09-02 20:49:00 +0700

    feat: themes setup

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "@/components/ui/atoms/ModeToggle";
 import { UserButton } from "@clerk/nextjs/app-beta";
 
 export default function Home() {
   return (
     <div className="">
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
