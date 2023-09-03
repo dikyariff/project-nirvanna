e0ef6c243fc5032004453229b15db58db3ae30bf
Diky Arif Febriyanto <dikyariff97@gmail.com>
2023-09-03 13:02:06 +0700

    chore: uploadthing setup

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -2,7 +2,9 @@
 
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+
 import {
   Dialog,
   DialogContent,
@@ -21,7 +23,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useEffect, useState } from "react";
+import { FileUpload } from "@/components/file-upload";
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -63,7 +65,7 @@ export const InitialModal = () => {
         <DialogContent className="bg-white text-black p-0 overflow-hidden">
           <DialogHeader className="pt-8 px-6">
             <DialogTitle className="text-2xl text-center font-bold">
-              Welcome to Project: Nirvanna 😁
+              Welcome to Project: Nirvanna 🥰
             </DialogTitle>
             <DialogDescription className="text-center text-zinc-500">
               Welcome aboard! Infuse your server with character by adding a name
@@ -74,7 +76,21 @@ export const InitialModal = () => {
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
               <div className="space-y-8 px-6">
                 <div className="flex items-center justify-center text-center">
-                  TODO: Image Upload
+                  <FormField
+                    control={form.control}
+                    name="imageUrl"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormControl>
+                          <FileUpload
+                            endpoint="serverImage"
+                            value={field.value}
+                            onChange={field.onChange}
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
                 </div>
 
                 <FormField
