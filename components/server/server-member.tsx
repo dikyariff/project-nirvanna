9918f00b95171b982b2634de4c92fe1877f8f684
Diky Arif Febriyanto <dikyariff97@gmail.com>
2023-09-11 10:57:45 +0700

    chore: fix display name member

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -29,23 +29,25 @@ export const ServerMember = ({ member, server }: ServerMemberProps) => {
   return (
     <button
       className={cn(
-        "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
+        "group px-2 py-2 rounded-md flex justify-between items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
         params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700"
       )}
     >
-      <UserAvatar
-        src={member.profile.imageUrl}
-        className="h-8 w-8 md:h-8 md:w-8"
-      />
-      <p
-        className={cn(
-          "font-medium text-sm ml-2 text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
-          params?.channelId === member.id &&
-            "text-primary dark:text-zinc-200 dark:group-hover:text-white"
-        )}
-      >
-        {member.profile.name}
-      </p>
+      <div className="flex items-center">
+        <UserAvatar
+          src={member.profile.imageUrl}
+          className="h-8 w-8 md:h-8 md:w-8"
+        />
+        <p
+          className={cn(
+            "line-clamp-1 truncate font-medium text-sm ml-2 text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
+            params?.channelId === member.id &&
+              "text-primary dark:text-zinc-200 dark:group-hover:text-white"
+          )}
+        >
+          {member.profile.name}
+        </p>
+      </div>
       {icon}
     </button>
   );
